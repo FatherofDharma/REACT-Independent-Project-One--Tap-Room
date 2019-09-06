710a07c6b4e820dc474dfed8178ef484c3c83e1a
FatherofDharma <fatherofdharma@gmail.com>
2019-09-06 09:42:05 -0700

    add App.jsx content and import it into index.jsx.

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function App(){
+  return (
+    <div>
+      <h1>Tap the Sky - Nectar of the gods on tap</h1>
+      <h3>Name: </h3>
+      <h3>Brewer: </h3>
+      <h3>Price: </h3>
+      <h3>ABV: </h3>
+      <p><em>Flavors: </em></p>
+    </div>
+  );
+}
+
+export default App;
