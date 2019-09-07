2e1c8e15a5130afdc0404598550cb31049563440
FatherofDharma <fatherofdharma@gmail.com>
2019-09-07 16:48:47 -0700

    add css object styling to header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header(){
+  var headerStyle = {
+    backgroundColor: 'black',
+    color: 'orange'
+  };
+  var headerLinkStyle = {
+    color: 'orange'
+  };
   return (
-    <div>
-      <h1 className="header">Tap the Sky</h1>
-      <style jsx>{`
-        .header {
-          background-color: black;
-          color: orange;
-        }
-      `}</style>
-      <Link to="/">Home</Link> | <Link to="/newtap">Add New Beer</Link>
+    <div style={headerStyle}>
+      <h1 >Tap the Sky</h1>
+      <Link to="/" style={headerLinkStyle}>Home</Link> | <Link to="/newtap" style={headerStyle}>Add New Beer</Link>
       <hr/>
     </div>
   );
