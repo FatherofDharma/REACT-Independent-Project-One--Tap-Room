5a4f4c9eb4fada2f90893b2f69bf5203b359979a
FatherofDharma <fatherofdharma@gmail.com>
2019-09-13 09:48:45 -0700

    add refs to newtapform and remove hard coded taps from ticketlist in preparation for lifting state to App.jsx.

--- src/components/NewTapForm.jsx
+++ src/components/NewTapForm.jsx
@@ -1,28 +1,52 @@
 import React from 'react';
 
 function NewTapForm(){
+  let _name = null;
+  let _brewer = null;
+  let _price = null;
+  let _abv = null;
+  let _flavors = null; 
+  
+  function handleNewTapFormSubmission(event) {
+    event.preventDefault();
+    console.log(_name.value);
+    console.log(_brewer.value);
+    console.log(_price.value);
+    console.log(_abv.value);
+    console.log(_flavors.value);
+    _name.value = '';
+    _brewer.value = '';
+    _price.value
+    _abv.value = '';
+    _flavors.value = '';
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={handleNewTapFormSubmission}>
         <input
           type='text'
           id='name'
-          placeholder='Beer Name'/>
+          placeholder='Beer Name'
+          ref={(input) => {_name = input;}}/>
         <input
           type='text'
           id='brewer'
-          placeholder='Brewer'/>
+          placeholder='Brewer'
+          ref={(input) => {_brewer = input;}}/>
         <input
           type='number'
           id='price'
-          placeholder='Price per Pint'/>
+          placeholder='Price per Pint'
+          ref={(input) => {_price = input;}}/>
         <input
           type='number'
           id='abv'
-          placeholder='ABV'/>
+          placeholder='ABV'
+          ref={(input) => {_abv = input;}}/>
         <textarea
           id='flavors'
-          placeholder='Flavor Description'/>
+          placeholder='Flavor Description'
+          ref={(textarea) => {_flavors = textarea;}}/>
         <button type='submit'>Create New Tap Entry</button>
       </form>
     </div>
--- src/components/TapList.jsx
+++ src/components/TapList.jsx
@@ -1,37 +1,6 @@
 import React from 'react';
 import Tap from './Tap';
 
-var masterTapList = [
-  {
-    name: 'Rain of Hops',
-    brewer: 'Cascadian River',
-    price: 5.00,
-    abv: 5.50,
-    flavors: 'Crisp fresh hops with a light taste of mint'
-  },
-  {
-    name: 'Orange You Glad You Have Taste',
-    brewer: 'Triple Pour',
-    price: 6.00,
-    abv: 6.50,
-    flavors: 'Like an Orangecicle cranked to eleven'
-  },
-  {
-    name: 'Hop on this Train',
-    brewer: 'Never Ending Brewery',
-    price: 7.00,
-    abv: 7.25,
-    flavors: 'Hoppy through and through'
-  },
-  {
-    name: 'Just a Sip Will do ya',
-    brewer: 'Cascadian River',
-    price: 4.00,
-    abv: 4.0,
-    flavors: 'A refreshing lager with hints of berries and almonds'
-  },
-];
-
 function TapList(){
   return (
     <div>
