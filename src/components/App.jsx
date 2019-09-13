1a09647692b82d634d4891d27c1985992bcb9bb5
FatherofDharma <fatherofdharma@gmail.com>
2019-09-13 10:19:02 -0700

    add state, masterTapList, and supporting callback function, and pass it down to newTapForm.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,22 +5,39 @@ import { Switch, Route } from 'react-router-dom';
 import NewTapForm from './NewTapForm';
 import Error404 from './Error404';
 
-function App(){
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTapList: []
+    };
+    this.handleAddingNewTapToList = this.handleAddingNewTapToList.bind(this);
+  }
+  
+  handleAddingNewTapToList(newTap){
+    let newMasterTapList = this.state.masterTapList.slice();
+    newMasterTapList.push(newTap);
+    this.setState({masterTapList: newMasterTapList});
+  }
+  
+  render() {
   var mainStyle = {
     // will add more styles here after learning a bit more about how the entire document is rendered.
     // backgroundColor: 'grey'
   };
 
-  return (
-    <div style={mainStyle}>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={TapList}/>
-        <Route path='/newtap' component={NewTapForm}/>
-        <Route component={Error404}/>
-      </Switch>
-    </div>
-  );
+    return (
+      <div style={mainStyle}>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={TapList}/>
+          <Route path='/newtap' render={() => <NewTapForm onNewTapCreation={this.handleAddingNewTapToList} />}/>
+          <Route component={Error404}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
